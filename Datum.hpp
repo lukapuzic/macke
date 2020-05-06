69d125f9cb42c657e61393b7b12851da53b5e69d
lukapuzic <64378850+lukapuzic@users.noreply.github.com>
2020-05-06 18:12:47 +0200

    Update Datum.hpp

--- Datum.hpp
+++ Datum.hpp
@@ -1,8 +1,19 @@
-
+  
 #ifndef DATUM_HPP_INCLUDED
 #define DATUM_HPP_INCLUDED
 
-class DATUM{
+class Datum{
+protected:
+    int dan;
+    int mesec;
+    int godina;
+public:
+    Datum(int d, int m, int g)
+    {
+    dan=d;
+    mesec=m;
+    godina=g;
+    }
 };
 
 #endif // DATUM_HPP_INCLUDED
