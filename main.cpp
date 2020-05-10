2be8fde5e69f65ce42ffaaf651e53b69ddf07bfd
lukapuzic <64378850+lukapuzic@users.noreply.github.com>
2020-05-10 21:52:29 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
+#include "Macka.hpp"//inkluduj sve klase
 
+int Oglas::ukupanbrojOglasa=0;
+
+void citajTxt(string nazivFajla)
+{
+    string linija;
+    ifstream fajl (nazivFajla);
+    if (fajl.is_open())
+    {
+        while ( getline (fajl,linija) )
+        {
+            cout << linija << '\n';
+        }
+        fajl.close();
+    }
+
+    else
+        cout << "Neuspesno otvoren fajl";
+
+}
 int main()
 {
-
+    citajTxt("ime.txt"); //
+ MACKA m;
+ Datum d;
+ PEDIGRE p;  
+    cout<<m<<endl<<d<<endl<<p<<endl;
     return 0;
 }
